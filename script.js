1693aa81222cfcbeb92485f33438ac5276311bdb
Patrick Ingram <114114167+JeepingCoder@users.noreply.github.com>
2022-11-20 00:10:22 -0500

    seudo coded, tried more broken code

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-const form = document.querySelector(".side");
- 
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  const inputVal = input.value;
-});
+//my api key
+// be3a1c546ba346651be769db457cb1b7
 
-const apiKey = "be3a1c546ba346651be769db457cb1b7";
-const inputVal = input.value;
- 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+//link the openweather API key to my page 
+var url = 'api.openweathermap.org/data/2.5/forecast?lat=35.2271&lon=80.8431&appid={be3a1c546ba346651be769db457cb1b7'
+var key = "be3a1c546ba346651be769db457cb1b7";
+var city = "YOUR CITY"; 
+var url = "https://api.openweathermap.org/data/2.5/forecast";
+//build a search box where user can submit a city 
 
-fetch(url)
-.then(response => response.json())
-.then(data => {
-  // do stuff with the data
-})
-.catch(() => {
-  msg.textContent = "Please search for a valid city 😩";
-});
+// var form = document.querySelector(".side");
+
+// console.log(form)
+// add a listener for the submit button
+ 
+// form.addEventListener("submit", e => {
+//   e.preventDefault();
+//   var inputVal = input.value;
+// });
+
+
+
+
+
+//have listener bring up current and future conditions for city searched 
+
+//add searched city to local storage to use in search history
+
+//display city search history on window
+
+//make city search history clickable 
+
+// pull saved citys from window storage 
+
+// when city search history clicked present current and future conditions
+
+//current weather conditions should load to the page along with City name, the Date 
+//an icon for weather conditions, the temp, the humidity, and wind speed 
+
+//have a way to look at future weather conditions in the 5 day forecast area
+
+//in the 5 day area display date, weather cond. icon, temp, wind speed and humidity
+
+
+
