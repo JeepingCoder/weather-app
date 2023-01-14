772a00bfed1b908e97f3f6b1fa3d053c6898a4cb
Patrick Ingram <114114167+JeepingCoder@users.noreply.github.com>
2023-01-14 10:32:10 -0500

    updates

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # weather-app
-Class project showing weather app 
+
+This is my Calender Application. It was a project given to us in our coding bootcamp. It was built to help a user schedule their day and to stay more organized. It could help the user with many aspects of life whether that be work, school or even help the user keep up with their families day to day activities. 
+
+This project challenged every bit of coding knowledge(which isn't much). Im learning to be patient and not to wait so long before asking for help.
+
+This project has been done many times over because of the coding boot camps, but I am certain mine looks different. I changed to lay out to make it a little more unique. 
--- script.js
+++ script.js
@@ -1,45 +1,125 @@
 //my api key
 // be3a1c546ba346651be769db457cb1b7
 
-//link the openweather API key to my page 
-var url = 'api.openweathermap.org/data/2.5/forecast?lat=35.2271&lon=80.8431&appid={be3a1c546ba346651be769db457cb1b7'
+// link the openweather API key to my page
+// var url = 'https://api.openweathermap.org/data/2.5/forecast?lat=35.2271&lon=80.8431&appid={be3a1c546ba346651be769db457cb1b7'
 var key = "be3a1c546ba346651be769db457cb1b7";
-var city = "YOUR CITY"; 
-var url = "https://api.openweathermap.org/data/2.5/forecast";
-//build a search box where user can submit a city 
+var city = "YOUR CITY";
+var searchBtn = document.querySelector(".citySearch");
+var inputValue = document.querySelector(".inputValue");
+var currentContainer = document.querySelector(".currentContainer");
+var forecastContainer = document.querySelector(".forecastContainer");
+// var requestUrl =
 
-// var form = document.querySelector(".side");
+//build a search box where user can submit a city- this is setting up for user input later just place holding
 
-// console.log(form)
-// add a listener for the submit button
- 
-// form.addEventListener("submit", e => {
-//   e.preventDefault();
-//   var inputVal = input.value;
-// });
+function getLatLng(city) {
+  var geo =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    city +
+    "&appid=be3a1c546ba346651be769db457cb1b7";
+  fetch(geo)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      var lat = data[0].lat;
+      var lon = data[0].lon;
 
+      getCurrent(lat, lon);
+      getForecast(lat, lon);
 
+      // call getforecast func and call the lat lng call fun get data here
+    });
+}
 
+function getCurrent(lat, lon) {
+  var forecast =
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
+    key;
+  fetch(forecast)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      var div = document.createElement("div");
 
+      var currentHtml = `
+      <h2>${data.name}</h2>
+      <p>Temp: ${1.8 * (data.main.temp - 273) + 32} F</p>
+      <p>Humidity: </p>
+      <p>Wind: </p>
+      `;
 
-//have listener bring up current and future conditions for city searched 
+      //display html block on a div
+      div.innerHTML = currentHtml;
+
+      //append the div to the currentContainer
+      currentContainer.appendChild(div);
+    });
+}
+
+function getForecast(lat, lon) {
+  var forecast =
+    "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
+    key;
+  fetch(forecast)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      for (var i = 0; i < data.list.length; i++) {
+        if (data.list[i].dt_txt.includes("12:00:00")) {
+          console.log(data.list[i]);
+
+          var forecastDate = data.list[i];
+          var div = document.createElement("div");
+
+          //create card html for each date
+          var forecastHtml = `
+          
+          `;
+
+          div.innerHTML = forecastHtml;
+          forecastContainer.appendChild(div);
+        }
+      }
+    });
+}
+
+searchBtn.addEventListener("submit", function (event) {
+  event.preventDefault();
+  var cityName = inputValue.value;
+  getLatLng(cityName);
+});
+
+// getForecast('forecast')
+
+// // seperate function for forecast witin data
+
+// // add a listener for the submit button
+
+//have listener bring up current and future conditions for city searched
 
 //add searched city to local storage to use in search history
 
 //display city search history on window
 
-//make city search history clickable 
+//make city search history clickable
 
-// pull saved citys from window storage 
+// pull saved citys from window storage
 
 // when city search history clicked present current and future conditions
 
-//current weather conditions should load to the page along with City name, the Date 
-//an icon for weather conditions, the temp, the humidity, and wind speed 
+//current weather conditions should load to the page along with City name, the Date
+//an icon for weather conditions, the temp, the humidity, and wind speed
 
 //have a way to look at future weather conditions in the 5 day forecast area
 
 //in the 5 day area display date, weather cond. icon, temp, wind speed and humidity
-
-
-
